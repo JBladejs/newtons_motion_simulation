b121c2355ec2f5cce887417cb6d8c384c8c50d7d
Bladejs <JBladejs@gmail.com>
2020-11-03 18:28:20 +0100

    Changed Game structure and implemented GameScreen class.

---
+++ core/src/com/game_physics/newtons_motion/GameScreen.kt
@@ -0,0 +1,22 @@
+package com.game_physics.newtons_motion
+
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
+
+class GameScreen(private val Game: Game) : Screen {
+    override fun render(delta: Float) {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+    }
+
+    override fun resize(width: Int, height: Int) {}
+
+    override fun dispose() {}
+
+    override fun pause() {}
+    override fun resume() {}
+    override fun hide() {}
+    override fun show() {}
+}
--- core/src/com/game_physics/newtons_motion/NewtonGame.kt
+++ core/src/com/game_physics/newtons_motion/NewtonGame.kt
@@ -1,29 +1,29 @@
 package com.game_physics.newtons_motion
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 
-class NewtonGame : ApplicationAdapter() {
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
+class NewtonGame : Game() {
+    lateinit var renderer: ShapeRenderer
+
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        renderer = ShapeRenderer()
+        renderer.setAutoShapeType(true)
+        screen = GameScreen(this)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
-        batch!!.end()
+        super.render()
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        img!!.dispose()
+        screen = null
+        screen.dispose()
+        renderer.dispose()
     }
 }
