e41f5351a99cda425a8e519c36fd2a800799062c
Rawaw <rafal.wawrzyniak@wir.pl>
2020-11-03 20:39:43 +0100

    Dodano opór powietrza

--- core/src/com/game_physics/newtons_motion/MovingObject.kt
+++ core/src/com/game_physics/newtons_motion/MovingObject.kt
@@ -4,22 +4,26 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import javax.swing.DesktopManager
 
 val grawitacja = 0.03f
-val tarcie = 0.2f  //strata prędkości od odbicia się od ściany (procentowa 1 - 100%    0.5 - 50%)
+val tarcie = 0.2f   //strata prędkości od odbicia się od ściany (procentowa 1 - 100%    0.5 - 50%)
+val opor = 0.01f    //opor powietrza zmniejszajacy stale predkosc pilki (procentowa)
 
-class MovingObject(private val radius: Float, private val color: Color, private var x: Float = 0.0f, private var y: Float = 0.0f, private var vx: Float = 2.0f, private var vy: Float = 2.0f) {
+class MovingObject(private val radius: Float, private val color: Color, private var x: Float = 0.0f, private var y: Float = 0.0f, private var vx: Float = 3.0f, private var vy: Float = 3.0f) {
     fun move(dt: Float) {
 
         x += vx*dt
         y += vy*dt
 
-
+        //opór powietrza
+        vx *= (1-opor)
+        vy *= (1-opor)
+        //grawitacja
         vy -= grawitacja*dt
 
         //TODO wyciągnąć aktualne wartości ekranu żeby nie wpisywać ograniczeń na sztywno
-        if(x <= 0 || x >= 640){
+        if(x < 0 || x > 640){
             vx *= (-1+tarcie)
         }
-        if(y <= 0 || y >= 480){
+        if(y < 0 || y > 480){
             vy *= (-1+tarcie)
             vx *= (1-tarcie)
         }
