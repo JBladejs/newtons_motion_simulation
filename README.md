a642218edcab65c50afd0922c3ce54c9ec84dab0
Bladejs <JBladejs@gmail.com>
2020-11-03 17:08:46 +0100

    Project init.

---
+++ README.md
@@ -0,0 +1,1 @@
+# newtons_motion_simulation
---
+++ desktop/src/com/game_physics/newtons_motion/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.game_physics.newtons_motion.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game_physics.newtons_motion.NewtonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NewtonGame(), config);
+	}
+}
