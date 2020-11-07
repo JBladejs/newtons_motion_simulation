e42f9657966de5e78a821981b88bafcb06ebfa54
Bladejs <JBladejs@gmail.com>
2020-11-07 14:54:14 +0100

    Refactor

--- core/src/com/game_physics/newtons_motion/GameScreen.kt
+++ core/src/com/game_physics/newtons_motion/GameScreen.kt
@@ -7,24 +7,24 @@ import com.badlogic.gdx.graphics.GL20
 
 class GameScreen(private val game: NewtonGame) : Screen {
     private var dt = 2f   //Prędkość gry
-    private val movingObject = MovingObject(10.0f, Color(255, 0, 0), Color(0, 255, 0), 50f, 50f)
+    private val playerObject = MovingObject(10.0f, Color(255, 0, 0), Color(0, 255, 0), 50f, 50f)
     private val target = TargetZone(Color(255, 0, 0), RNG.nextX(25f), RNG.nextY(25f), 50f, 50f)
     private val rotationSpeed = 2.5f
 
     private fun update() {
-        movingObject.move(dt)
+        playerObject.move(dt)
 
         //wykrywanie kolizji pilki z polem docelowym
-        if (target.contains(movingObject.x - movingObject.radius, movingObject.y + movingObject.radius) &&
-                target.contains(movingObject.x + movingObject.radius, movingObject.y - movingObject.radius)) {
+        if (target.contains(playerObject.x - playerObject.radius, playerObject.y + playerObject.radius) &&
+                target.contains(playerObject.x + playerObject.radius, playerObject.y - playerObject.radius)) {
             target.color = Color(0, 255, 0)
-            movingObject.stop()
+            playerObject.stop()
         }
 
-        if (Gdx.input.isKeyPressed(W)) movingObject.increaseSpeed()
-        if (Gdx.input.isKeyPressed(S)) movingObject.decreaseSpeed()
-        if (Gdx.input.isKeyPressed(A)) movingObject.rotation -= rotationSpeed
-        if (Gdx.input.isKeyPressed(D)) movingObject.rotation += rotationSpeed
+        if (Gdx.input.isKeyPressed(W)) playerObject.increaseSpeed()
+        if (Gdx.input.isKeyPressed(S)) playerObject.decreaseSpeed()
+        if (Gdx.input.isKeyPressed(A)) playerObject.rotation -= rotationSpeed
+        if (Gdx.input.isKeyPressed(D)) playerObject.rotation += rotationSpeed
     }
 
     override fun render(delta: Float) {
@@ -32,7 +32,7 @@ class GameScreen(private val game: NewtonGame) : Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         with(game.renderer) {
             begin()
-            movingObject.render(this)
+            playerObject.render(this)
             target.render(this)
             end()
         }
