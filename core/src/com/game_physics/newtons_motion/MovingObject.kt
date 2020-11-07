2a9710391fbbba6a50cdf9dc0249ff1b6cb1d1dc
Bladejs <JBladejs@gmail.com>
2020-11-07 01:03:44 +0100

    Refactor.

--- core/src/com/game_physics/newtons_motion/MovingObject.kt
+++ core/src/com/game_physics/newtons_motion/MovingObject.kt
@@ -2,11 +2,10 @@ package com.game_physics.newtons_motion
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
-import javax.swing.DesktopManager
 
-val grawitacja = 0.03f
-val tarcie = 0.2f   //strata prędkości od odbicia się od ściany (procentowa 1 - 100%    0.5 - 50%)
-val opor = 0.01f    //opor powietrza zmniejszajacy stale predkosc pilki (procentowa)
+const val gravity = 0.03f
+const val flexibility = 0.2f   //strata prędkości od odbicia się od ściany (procentowa 1 - 100%    0.5 - 50%)
+const val resistance = 0.01f    //opor powietrza zmniejszajacy stale predkosc pilki (procentowa)
 
 class MovingObject(private val radius: Float, private val color: Color, private var x: Float = 0.0f, private var y: Float = 0.0f, var vx: Float = 0.0f, var vy: Float = 0.0f) {
     fun move(dt: Float) {
@@ -15,21 +14,21 @@ class MovingObject(private val radius: Float, private val color: Color, private 
         y += vy*dt
 
         //opór powietrza
-        vx *= (1-opor)
-        vy *= (1-opor)
+        vx *= (1-resistance)
+        vy *= (1-resistance)
         //grawitacja
-        vy -= grawitacja*dt
+        vy -= gravity*dt
 
         if (x - radius < 0 || x + radius > Gdx.graphics.width){
             x = if (x - radius < 0) radius
             else Gdx.graphics.width - radius
-            vx *= (-1+tarcie)
+            vx *= (-1+flexibility)
         }
         if (y - radius < 0 || y + radius > Gdx.graphics.height){
             y = if (y - radius < 0) radius
             else Gdx.graphics.height - radius
-            vy *= (-1+tarcie)
-            vx *= (1-tarcie)
+            vy *= (-1+flexibility)
+            vx *= (1-flexibility)
         }
     }
 
